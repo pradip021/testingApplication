78d165975dcd7144573c52ae15dc1432ef65cf28
pradip021 <deepakpal8530@gmail.com>
2025-12-09 11:35:26 +0530

    Added route

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const PORT = process.env.PORT || 3000;
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
+app.get('/pradip', (req, res) => {
+  res.send('Hello World!');
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
